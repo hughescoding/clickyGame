e6c8742f9c980eb4d2911383b38cd72db7b4358d
hughescoding <mhughes@myjao.org>
2019-01-17 20:30:31 -0500

    Updates

---
+++ precache-manifest.a08999d86737ec5613fc8318bbb21f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "429bd4b716c91366bf40",
+    "url": "/clickyGame/static/js/main.429bd4b7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickyGame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "429bd4b716c91366bf40",
+    "url": "/clickyGame/static/css/main.771383a7.chunk.css"
+  },
+  {
+    "revision": "7a0ccfdf995be15ceac76db89f6e5f57",
+    "url": "/clickyGame/index.html"
+  }
+];
